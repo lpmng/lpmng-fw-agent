6f97e549900abb385f95f403dd7f4421caa90aa1
Gaël Berthaud-Müller <blacksponge@tuta.io>
2018-01-31 18:36:54 +0100

    Parsing json to event session

--- fw.go
+++ fw.go
@@ -1,35 +1,54 @@
 package main
 
 import(
-	"fmt"
-	"net/http"
-	"encoding/json"
+    "fmt"
+    "net/http"
+    "encoding/json"
 )
 
+type Session struct {
+    Mac      string `json:"max"`
+    Ip4      string `json:"ip4"`
+    Internet bool   `json:"internet"`
+    User     int    `json:"user"`
+}
+
+type EventSession struct {
+    Action  string  `json:"action"`
+    Name    string  `json:"name"`
+    Session Session `json:"param"`
+}
 
 func echoHandler(w http.ResponseWriter, r *http.Request) {
-        if r.Method != "POST" {
-          http.Error(w, "Invalid request method.", 405)
-          return
-        }
+    if r.Method != "POST" {
+        http.Error(w, "Invalid request method.", 405)
+        return
+    }
 
-        var event map[string]interface{}
-	err := json.NewDecoder(r.Body).Decode(&event)
+    var event EventSession
+    err := json.NewDecoder(r.Body).Decode(&event)
 
-        if err != nil {
-		http.Error(w, "Invalid json", 400)
-                return
-	}
+    if err != nil {
+        http.Error(w, err.Error(), 400)
+        return
+    }
 
-	fmt.Println(event)
+    switch event.Action {
+        case "created", "updated" :
+            fmt.Println(event.Session.Ip4)
+            fmt.Println(event.Session.Internet)
+            fmt.Println(event.Action)
+        default:
+            http.Error(w, "Wrong action type. Supported action are 'created', 'updated'.", 400)
+    }
 }
 
 
 
 func main(){
-	http.HandleFunc("/event/session", echoHandler)
+    http.HandleFunc("/event/session", echoHandler)
 
-	http.ListenAndServe("0.0.0.0:8000", nil)
+    http.ListenAndServe("0.0.0.0:8000", nil)
 }
 
 
